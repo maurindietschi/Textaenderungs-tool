4521217787a111f4ec3a213696795b5e3492ddb6
Lars Stalder <88284345+LordofCoding06@users.noreply.github.com>
2023-05-25 08:38:55 +0200

    Many changes

--- anzeigen.ps1
+++ anzeigen.ps1
@@ -6,11 +6,12 @@ function showmenu {
   Write-Host "=== Vergleichs Tool ==="
   Write-Host "1. Option 1"
   Write-Host "2. Option 2"
-  Write-Host "3. Durchsuchen und Datei auswählen"
+  Write-Host "3. Durchsuchen und Datei auswaehlen"
   Write-Host "4. Beenden"
 }
 
 function Get-FilePath {
+  Clear-Host
   Param (
     [string]$folderPath
   )
@@ -25,7 +26,9 @@ function Get-FilePath {
     $index = 1
     $selection = 0
 
-    Write-Host "Current path: $currentPath"
+    Write-Host "Momentaner Pfad: $currentPath"
+    Write-Host ""
+    Write-Host "---------------"
     foreach ($item in $items) {
       $itemName = $item.Name
       
@@ -34,7 +37,7 @@ function Get-FilePath {
       $index++
     }
 
-    $selection = Read-Host "Wählen Sie eine Option (1 - $($index - 1))  oder 'z' um zum vorherigen Pfad zurückzukehren, oder geben Sie 'q' ein um abzubrechen."
+    $selection = Read-Host "Waehlen Sie eine Option (1 - $($index - 1))  oder 'z' um zum vorherigen Pfad zurueckzukehren, oder geben Sie 'q' ein um abzubrechen."
 
     if ($selection -eq "q") {
       return $currentPath
@@ -58,38 +61,39 @@ function Get-FilePath {
       }
     }
     else {
-      Write-Host "Ungültige Auswahl!"
+      Write-Host "Ungueltige Auswahl!"
     }
   }
 }
 
 while ($selection -ne "4") {
   showmenu
-  $selection = Read-Host "Wählen Sie eine Option aus (1-4)"
+  $selection = Read-Host "Waehlen Sie eine Option aus (1-4)"
 
   switch ($selection) {
     "1" {
-      Write-Host "Option 1 ausgewählt"
-      Read-Host "Drücken Sie eine beliebige Taste, um fortzufahren..."
+      Write-Host "Option 1 ausgewaehlt"
+      Read-Host "Druecken Sie eine beliebige Taste, um fortzufahren..."
     }
     "2" {
-      Write-Host "Option 2 ausgewählt"
-      Read-Host "Drücken Sie eine beliebige Taste, um fortzufahren..."
+      Write-Host "Option 2 ausgewaehlt"
+      Read-Host "Druecken Sie eine beliebige Taste, um fortzufahren..."
     }
     "3" {
-      Write-Host "Durchsuchen und Datei auswählen"
+      Write-Host "Durchsuchen und Datei auswaehlen"
       $selectedFilePath = Get-FilePath -folderPath $folderPath
 
       Clear-Host
-      Write-Host "Ausgewählter Dateipfad: $selectedFilePath"
-      Read-Host "Drücken Sie eine beliebige Taste, um fortzufahren..."
+      Write-Host "Ausgewaehlter Dateipfad: $selectedFilePath"
+      $folderPath = $selectedFilePath
+      Read-Host "Druecken Sie eine beliebige Taste, um fortzufahren..."
     }
     "4" {
 
     }
     default {
-      Write-Host "Ungültige Auswahl! Bitte geben Sie eine gültige Option ein."
-      Read-Host "Drücken Sie eine beliebige Taste, um fortzufahren..."
+      Write-Host "Ungueltige Auswahl! Bitte geben Sie eine gueltige Option ein."
+      Read-Host "Druecken Sie eine beliebige Taste, um fortzufahren..."
     }
   }
 }
